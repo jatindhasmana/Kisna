bf5ec79a94c9cfcd0b4ff8fab8366966096df5f5
Krishna Raj Singh <krishnarajsingh17012003@gmail.com>
2024-02-06 19:06:11 +0530

    Third commit

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require("path")
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderLust';
-const Listing = require("./models/listing.js")
+const Listing = require("./models/listing.js");
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js");
+
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -28,60 +31,68 @@ app.get("/", (req, res)=>{
 });
 
 //Index Route
-app.get("/listings", async (req, res)=>{
+app.get("/listings", wrapAsync(async (req, res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-})
+}));
 
 //New Route
 app.get("/listings/new", (req, res)=>{
     res.render("listings/new.ejs");
-})
+});
 
 
 //Show Route
-app.get("/listings/:id", async (req, res)=>{
+app.get("/listings/:id", wrapAsync(async (req, res)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing});
-})
+}));
 
 //Create Route
-app.post("/listings", (req, res, next)=>{
-    try {
-        const newListing = new Listing(req.body);
-        newListing.save();
-        res.redirect("/listings");
-    } catch (error) {
-        next(error)
-    }
-})
+app.post("/listings", wrapAsync(async (req, res)=>{
+
+    const newListing = new Listing(req.body);
+    await newListing.save();
+    res.redirect("/listings");
+  
+}))
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res)=>{
+app.get("/listings/:id/edit", wrapAsync(async (req, res)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id)
     res.render("listings/edit.ejs", { listing })
-})
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res)=>{
+app.put("/listings/:id", wrapAsync(async (req, res)=>{
     let{title, description, image, price, location, country} = req.body;
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {title: title, description: description, image: image, price: price, location: location, country: country });
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res)=>{
+app.delete("/listings/:id", wrapAsync(async (req, res)=>{
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
 
-app.use((err, req, res, next)=>{
-    res.send("Something went wrong")
+app.all("*", (req, res, next)=>{
+    next(new ExpressError(404, "Page not Found!"))
 })
+
+// app.use((err, req, res, next)=>{
+//     let { statusCode=500,  message="something went wrong" } = err;
+//     err = (message.split(",")[0].split("Path")[1])
+//     // res.status(statusCode).render("error.ejs", { message: err})
+//     // res.status(statusCode).send(message)
+
+//     next()
+    
+// })
 
 app.listen(8080, ()=>{
     console.log("server is listenting on port 8080")
--- models/listing.js
+++ models/listing.js
@@ -14,9 +14,18 @@ const listingScema = Schema({
         set: (v)=> v === "" ? "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" : v,
 
     }, 
-    price: Number, 
-    location: String, 
-    country: String, 
+    price: {
+        type: Number,
+        min: [0, "Please enter valid price"]
+    }, 
+    location: {
+        type: String,
+        required: true
+    }, 
+    country: {
+        type: String,
+        required: true
+    }, 
 });
 
 const Listing = mongoose.model("Listing", listingScema);
